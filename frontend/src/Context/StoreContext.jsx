9c2b66f9720414b589452b9720079390300b2e93
Storyshooter <133217060+storyshooter@users.noreply.github.com>
2024-08-15 09:43:45 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://food-del-backend-vggd.onrender.com"
+    const url = "https://foodpocket-entri-project-backend.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
